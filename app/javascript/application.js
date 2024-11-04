024bd6c25454f515acc48ef3f5780f3082cad183
Dennis Schreiner <DennisSchreiner96@gmail.com>
2024-11-04 21:25:01 +0100

    Add Bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./controllers"
 import "./stylesheets/application.sass"
